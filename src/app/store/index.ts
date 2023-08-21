d936034be5c72dd587cc2a884a2c0c27e9b88ef4
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2023-08-21 12:44:00 -0500

    add imgSrc to bookmark

--- src/app/store/index.ts
+++ src/app/store/index.ts
@@ -38,6 +38,8 @@ export const bookmarksAtom = atomWithStorage<Array<BookmarkType>>('bookmarks', [
 		id: 6,
 		title: 'YouTube',
 		url: 'https://youtube.com',
+		imgSrc:
+			'https://images.unsplash.com/photo-1521302200778-33500795e128?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80',
 		featured: true,
 		tags: ['video streaming'],
 	},
--- src/app/types/index.ts
+++ src/app/types/index.ts
@@ -2,6 +2,7 @@ export type BookmarkType = {
 	id: number;
 	title: string;
 	url: string;
+	imgSrc?: string;
 	featured?: boolean;
 	tags?: Array<string>;
 };
