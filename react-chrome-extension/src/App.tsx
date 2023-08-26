102935af1ce775c74b6674ea6ff43683c3ca101e
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2023-08-25 21:42:16 -0500

    basic tailwind styling

--- react-chrome-extension/src/App.tsx
+++ react-chrome-extension/src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
-    <main>
-      <h1>Hello from Vite!</h1>
+    <main className="rounded-md py-8 m-8 flex flex-col w-96 items-center border border-white">
+      <h1 className="text-3xl font-bold text-center">Hello from Vite!</h1>
     </main>
   );
 }
