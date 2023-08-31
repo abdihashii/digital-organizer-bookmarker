26e251551353b1bb23062fb38f2ba90663d05096
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2023-08-31 14:56:38 +0000

    Removing unused code

--- client/src/app/api/get-screenshot/route.ts
+++ client/src/app/api/get-screenshot/route.ts
@@ -1,4 +1,3 @@
-// import { NextResponse } from 'next/server';
 import { v2 as cloudinary } from 'cloudinary';
 import puppeteer from 'puppeteer-core'
 import {
@@ -30,9 +29,6 @@ export async function GET(request: Request) {
 	  })
 	const page = await browser.newPage();
 	await page.goto(url);
-
-	// Wait for the page to load
-	// await page.waitForLoadState('domcontentloaded');
 
 	const sanitizedUrl = sanitizeUrlForPublicId(url);
 	const publicId = `screenshot-${sanitizedUrl}`;
