89cf9b91fb24dd7b9aa52343b2abe8f9b49a35ab
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2023-09-02 14:03:51 -0500

    Add dashboard button to home screen

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,3 +1,16 @@
+import Link from 'next/link';
+
 export default function Home() {
-  return <main className="">Hello, World!</main>;
+	return (
+		<main className="flex min-h-screen flex-col gap-8 bg-gray-200 p-12">
+			<h1>Hello, World!</h1>
+
+			<Link
+				href="/dashboard"
+				className="w-fit rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"
+			>
+				Dashboard
+			</Link>
+		</main>
+	);
 }
