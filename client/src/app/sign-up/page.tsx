9d6d1b939814b8ce9078d46a5d11451a6dc973df
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2023-09-04 17:14:25 -0500

    Sign up flow

--- client/src/app/sign-up/page.tsx
+++ client/src/app/sign-up/page.tsx
@@ -1,16 +1,50 @@
+'use client';
+
 import React from 'react';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Button } from '@/components/ui/button';
 import Link from 'next/link';
+// import Image from 'next/image';
+import useAuth from '@/app/hooks/useAuth';
 
 export default function SignUpPage() {
+	const { signUp } = useAuth();
+
+	const handleSignUp = async (event: React.FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+
+		const target = event.target as typeof event.target & {
+			email: { value: string };
+			password: { value: string };
+			confirmPassword: { value: string };
+		};
+
+		const email = target.email.value;
+		const password = target.password.value;
+		const confirmPassword = target.confirmPassword.value;
+
+		if (password !== confirmPassword) {
+			alert("Passwords don't match");
+			return;
+		}
+
+		await signUp(email, password);
+	};
+
 	return (
 		<main className="h-screen w-full md:flex md:flex-row md:items-center lg:p-20 xl:p-28">
 			<article className="hidden h-full w-2/3 items-center bg-gray-100 px-10 md:flex">
 				<p className="text-3xl font-bold text-gray-900">
 					Lorem ipsum dolor sit amet consectetur adipisicing elit.
 				</p>
+
+				{/* <Image
+					src="/ben-kim-HeSAWmZ_tqw-unsplash.jpg"
+					width={'100%'}
+					height={'100%'}
+					alt="a snow covered mountain with a sky background"
+				/> */}
 			</article>
 
 			<article className="flex h-full flex-col justify-center gap-8 px-10 md:w-1/2">
@@ -18,10 +52,14 @@ export default function SignUpPage() {
 					Sign Up
 				</h1>
 
-				<form className="flex flex-col gap-8">
+				<form
+					className="flex flex-col gap-8"
+					onSubmit={handleSignUp}
+				>
 					<section>
 						<Label htmlFor="email">Email</Label>
 						<Input
+							required
 							id="email"
 							type="email"
 						/>
@@ -30,6 +68,7 @@ export default function SignUpPage() {
 					<section>
 						<Label htmlFor="password">Password</Label>
 						<Input
+							required
 							id="password"
 							type="password"
 						/>
@@ -38,6 +77,7 @@ export default function SignUpPage() {
 					<section>
 						<Label htmlFor="confirmPassword">Confirm Password</Label>
 						<Input
+							required
 							id="confirmPassword"
 							type="password"
 						/>
@@ -58,7 +98,7 @@ export default function SignUpPage() {
 
 					<Link
 						className="text-center text-blue-500 hover:underline"
-						href="/sign-in"
+						href="/log-in"
 					>
 						Log In
 					</Link>
