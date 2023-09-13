7449017d557cea72cfbc4ab4c0f9cd5e1de676d2
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2023-09-13 09:57:54 -0500

    Makes dashboard page a server component

--- client/src/app/dashboard/page.tsx
+++ client/src/app/dashboard/page.tsx
@@ -1,24 +1,25 @@
-'use client';
-
-import React, { useState } from 'react';
+import React from 'react';
 import SearchBookmarks from '../components/searchBookmarks';
-import Bookmark from '../components/bookmark';
-import { BookmarkType } from '@/app/types';
-import BookmarkModal from '@/app/components/bookmarkModal';
-import { useBookmarks } from '@/app/hooks/useBookmarks';
 import LeftSideNav from '@/app/components/nav';
+import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
+import Bookmarks from './bookmarks';
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
+import { Database } from '../types/supabase.types';
 
-export default function Dashboard() {
-	// Fetch bookmarks from supabase and filter them using our custom hook
-	const { featuredBookmarks, restOfBookmarks } = useBookmarks();
-
-	const [bookmarkModal, setBookmarkModal] = useState<{
-		isOpen: boolean;
-		bookmark: BookmarkType | null;
-	}>({
-		isOpen: false,
-		bookmark: null,
+export default async function Dashboard() {
+	const supabase = createServerComponentClient<Database>({
+		cookies,
 	});
+
+	const {
+		data: { session },
+	} = await supabase.auth.getSession();
+
+	if (!session) {
+		// this is a protected route - only users who are signed in can view this route
+		redirect('/sign-in');
+	}
 
 	return (
 		<main className="flex h-screen flex-col md:flex-row">
@@ -32,49 +33,7 @@ export default function Dashboard() {
 				<div className="flex flex-col gap-8 lg:w-10/12">
 					<SearchBookmarks />
 
-					{/* Featured Bookmarks List */}
-					<section className="flex flex-col gap-4">
-						<h2 className="text-2xl font-bold text-gray-800">
-							Featured Bookmarks
-						</h2>
-
-						<div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
-							{featuredBookmarks.map((bookmark) => (
-								<Bookmark
-									key={bookmark.uuid}
-									bookmark={bookmark}
-									bookmarkModal={bookmarkModal}
-									setBookmarkModal={setBookmarkModal}
-								/>
-							))}
-						</div>
-					</section>
-
-					<hr className="border-gray-400" />
-
-					{/* Rest of Bookmarks List */}
-					<section className="flex w-full flex-col gap-4">
-						<h2 className="text-2xl font-bold text-gray-800">Bookmarks</h2>
-
-						<div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
-							{restOfBookmarks.map((bookmark) => (
-								<Bookmark
-									key={bookmark.uuid}
-									bookmark={bookmark}
-									bookmarkModal={bookmarkModal}
-									setBookmarkModal={setBookmarkModal}
-								/>
-							))}
-						</div>
-					</section>
-
-					{/* Bookmark Modal */}
-					{bookmarkModal.isOpen && (
-						<BookmarkModal
-							bookmarkModal={bookmarkModal}
-							setBookmarkModal={setBookmarkModal}
-						/>
-					)}
+					<Bookmarks />
 				</div>
 			</article>
 		</main>
