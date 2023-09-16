13232fbd8cf5a357a19ce9c562b3b72e011637fd
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2023-09-16 12:25:39 -0500

    fix up the middleware

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -1,13 +1,16 @@
 import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs';
-import { NextRequest, NextResponse } from 'next/server';
 import { Database } from './app/types/supabase.types';
+import { NextRequest, NextResponse } from 'next/server';
 
 export async function middleware(req: NextRequest) {
 	const res = NextResponse.next();
-	const supabase = createMiddlewareClient<Database>({ req, res });
-	const { data, error } = await supabase.auth.getSession();
 
-	console.log(`Middleware data: ${JSON.stringify({ data })}`);
+	const supabaseMiddlewareClient = createMiddlewareClient<Database>({
+		req,
+		res,
+	});
+
+	const { error } = await supabaseMiddlewareClient.auth.getSession();
 
 	if (error) {
 		console.log(`Middleware error: ${JSON.stringify({ error })}`);
@@ -15,3 +18,7 @@ export async function middleware(req: NextRequest) {
 
 	return res;
 }
+
+export const config = {
+	matcher: ['/', '/dashboard'],
+};
