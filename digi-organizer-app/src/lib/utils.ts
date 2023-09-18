52895d94cb5c94507b28463953e174596236fb64
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2023-09-18 11:19:29 -0500

    ViewBookmark component

--- digi-organizer-app/src/lib/utils.ts
+++ digi-organizer-app/src/lib/utils.ts
@@ -1,6 +1,27 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
+import { type ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const removeTrailingSlashFromUrl = (url: string) => {
+  return url.replace(/\/$/, '');
+};
+
+export const removeWrappingQuotesFromUrl = (url: string) => {
+  return url.replace(/['"]+/g, '');
+};
+
+export const cleanUpImgSrc = (url: string) => {
+  if (!url) {
+    return null;
+  }
+
+  const urlWithoutTrailingSlash = removeTrailingSlashFromUrl(url);
+  const urlWithoutWrappingQuotes = removeWrappingQuotesFromUrl(
+    urlWithoutTrailingSlash
+  );
+
+  return urlWithoutWrappingQuotes;
+};
