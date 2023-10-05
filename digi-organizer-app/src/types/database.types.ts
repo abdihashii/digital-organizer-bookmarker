d6c7435544e0211a707b23eeee586bd79f5ce527
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2023-10-05 16:55:18 -0500

    update supabase database types

--- digi-organizer-app/src/types/database.types.ts
+++ digi-organizer-app/src/types/database.types.ts
@@ -11,28 +11,34 @@ export interface Database {
     Tables: {
       bookmarks: {
         Row: {
+          created_at: string
           featured: boolean | null
           imgsrc: string | null
           tags: string[] | null
           title: string | null
+          updated_at: string
           url: string | null
           user_id: string | null
           uuid: string
         }
         Insert: {
+          created_at?: string
           featured?: boolean | null
           imgsrc?: string | null
           tags?: string[] | null
           title?: string | null
+          updated_at?: string
           url?: string | null
           user_id?: string | null
           uuid?: string
         }
         Update: {
+          created_at?: string
           featured?: boolean | null
           imgsrc?: string | null
           tags?: string[] | null
           title?: string | null
+          updated_at?: string
           url?: string | null
           user_id?: string | null
           uuid?: string
