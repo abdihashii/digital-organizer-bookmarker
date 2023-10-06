beab3741467d5a4353689dfbdcc33bd242e05247
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2023-10-06 12:50:55 -0500

    Deleting the old client code

--- client/src/app/types/index.ts
+++
@@ -1,8 +0,0 @@
-export type BookmarkType = {
-	uuid: number | string;
-	title: string | null;
-	url: string | null;
-	imgsrc?: string | null;
-	featured?: boolean | null;
-	tags?: Array<string> | null;
-};
--- client/src/lib/supabaseClient.ts
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-import { Database } from '@/app/types/supabase.types';
-
-// Create a single supabase client for interacting with your database
-export const supabase = createClient<Database>(
-	process.env.NEXT_PUBLIC_SUPABASE_URL as string,
-	process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string,
-);
