71301d96f9f81441f229a5603da5fab52914aa97
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2023-10-07 17:52:01 -0500

    Can now generate tags when adding a new bookmarks

--- digi-organizer-app/src/components/AddBookmarkModal.tsx
+++ digi-organizer-app/src/components/AddBookmarkModal.tsx
@@ -6,8 +6,11 @@ import {
   createClientComponentClient,
 } from '@supabase/auth-helpers-nextjs';
 import React, { useState } from 'react';
-import ToolTip from './Tooltip';
+import ToolTip from '@/components/Tooltip';
 import { useRouter } from 'next/navigation';
+import type { NewBookmarkType } from '@/types/BookmarkType';
+import { XCircle } from 'lucide-react';
+import { Skeleton } from '@/components/ui/skeleton';
 
 const AddBookmarkModal = ({
   user,
@@ -18,13 +21,38 @@ const AddBookmarkModal = ({
 }) => {
   const supabase = createClientComponentClient();
   const router = useRouter();
-  const [newBookmark, setNewBookmark] = useState({
+  const [generatingTags, setGeneratingTags] = useState<boolean>(false);
+  const [newBookmark, setNewBookmark] = useState<NewBookmarkType>({
     title: '',
     url: '',
     featured: false,
-    // tags: [],
+    tags: [],
     user_id: '',
   });
+
+  const handleGenerateTags = async () => {
+    // reset tags to empty array
+    setNewBookmark({ ...newBookmark, tags: [] });
+
+    setGeneratingTags(true);
+
+    const res = await fetch(
+      `http://localhost:3000/api/tagifier?url=${newBookmark.url}`
+    );
+
+    const { error, themes, specifics } = await res.json();
+
+    if (error) {
+      alert(error.message);
+      return;
+    }
+
+    setGeneratingTags(false);
+
+    const tags = [...themes, ...specifics];
+
+    setNewBookmark({ ...newBookmark, tags });
+  };
 
   const handleFormSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -42,7 +70,7 @@ const AddBookmarkModal = ({
       title: '',
       url: '',
       featured: false,
-      // tags: [],
+      tags: [],
       user_id: '',
     });
 
@@ -69,7 +97,7 @@ const AddBookmarkModal = ({
             placeholder="Title"
             id="bookmarkTitle"
             autoFocus
-            value={newBookmark.title}
+            value={newBookmark.title as string}
             onChange={(e) =>
               setNewBookmark({ ...newBookmark, title: e.target.value })
             }
@@ -90,7 +118,7 @@ const AddBookmarkModal = ({
             className="rounded-md border border-gray-300 p-4"
             placeholder="URL"
             id="bookmarkURL"
-            value={newBookmark.url}
+            value={newBookmark.url as string}
             onChange={(e) =>
               setNewBookmark({ ...newBookmark, url: e.target.value })
             }
@@ -109,21 +137,74 @@ const AddBookmarkModal = ({
             type="checkbox"
             className="rounded-md border border-gray-300 p-4"
             id="featureBookmark"
-            checked={newBookmark.featured}
+            checked={newBookmark.featured as boolean}
             onChange={(e) =>
               setNewBookmark({ ...newBookmark, featured: e.target.checked })
             }
           />
         </section>
 
+        {/* Tags */}
+        <section className="flex flex-col gap-2">
+          {newBookmark.tags && (
+            <div className="flex flex-col gap-2">
+              <label
+                htmlFor="bookmarkTags"
+                className="text-gray-800 dark:text-gray-300"
+              >
+                Tags:
+              </label>
+
+              {/* Generating tags loading skeleton */}
+              {generatingTags && (
+                <div className="flex flex-row flex-wrap gap-2">
+                  <Skeleton className="whitespace-nowrap rounded-md border bg-gray-200 px-6 py-3 text-xs text-gray-700 group-hover:cursor-default dark:bg-gray-700 dark:text-white dark:group-hover:border-gray-500" />
+                  <Skeleton className="whitespace-nowrap rounded-md border bg-gray-200 px-6 py-3 text-xs text-gray-700 group-hover:cursor-default dark:bg-gray-700 dark:text-white dark:group-hover:border-gray-500" />
+                  <Skeleton className="whitespace-nowrap rounded-md border bg-gray-200 px-6 py-3 text-xs text-gray-700 group-hover:cursor-default dark:bg-gray-700 dark:text-white dark:group-hover:border-gray-500" />
+                  <Skeleton className="whitespace-nowrap rounded-md border bg-gray-200 px-6 py-3 text-xs text-gray-700 group-hover:cursor-default dark:bg-gray-700 dark:text-white dark:group-hover:border-gray-500" />
+                  <Skeleton className="whitespace-nowrap rounded-md border bg-gray-200 px-6 py-3 text-xs text-gray-700 group-hover:cursor-default dark:bg-gray-700 dark:text-white dark:group-hover:border-gray-500" />
+                </div>
+              )}
+
+              <div className="flex flex-row flex-wrap gap-2">
+                {newBookmark.tags.map((tag, i) => (
+                  <div
+                    key={`${tag}-${newBookmark.url}-${i}`}
+                    className="group relative"
+                  >
+                    <span className="whitespace-nowrap rounded-md border bg-gray-200 px-2 py-1 text-xs text-gray-700 group-hover:cursor-default dark:bg-gray-700 dark:text-white dark:group-hover:border-gray-500">
+                      {tag}
+                    </span>
+                    {/* delete circle button */}
+                    <XCircle
+                      className="invisible absolute -right-2.5 -top-2.5 cursor-pointer text-gray-700 group-hover:visible"
+                      size={24}
+                      onClick={() => {
+                        const tags = newBookmark.tags?.filter(
+                          (t) => t !== tag
+                        ) as string[];
+
+                        setNewBookmark({ ...newBookmark, tags });
+                      }}
+                      fill="white"
+                    />
+                  </div>
+                ))}
+              </div>
+            </div>
+          )}
+        </section>
+
         {/* Generate Tags button */}
         <ToolTip
           triggerContent={
             <button
+              type="button"
               className={`rounded-md bg-orange-500 p-4 text-white
             ${newBookmark.url === '' ? 'cursor-not-allowed opacity-50' : ''}
           `}
               disabled={newBookmark.url === ''}
+              onClick={handleGenerateTags}
             >
               Generate Tags
             </button>
--- digi-organizer-app/src/types/BookmarkType.ts
+++ digi-organizer-app/src/types/BookmarkType.ts
@@ -1,9 +1,11 @@
 export type BookmarkType = {
   featured: boolean | null;
-  imgsrc: string | null;
+  imgsrc?: string | null;
   tags: string[] | null;
   title: string | null;
   url: string | null;
   user_id: string | null;
   uuid: string;
 };
+
+export type NewBookmarkType = Omit<BookmarkType, 'uuid'>;
