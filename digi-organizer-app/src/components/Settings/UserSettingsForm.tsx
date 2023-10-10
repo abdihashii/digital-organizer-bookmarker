50c707812e526cba6e3cd97105b4b44ca6ade059
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2023-10-09 20:04:35 -0500

    Fix up the look of the user settings form

--- digi-organizer-app/src/components/Settings/UserSettingsForm.tsx
+++ digi-organizer-app/src/components/Settings/UserSettingsForm.tsx
@@ -55,18 +55,19 @@ const UserSettingsForm = ({ profile }: { profile: ProfileType }) => {
     >
       <h2 className="text-2xl font-bold">User Settings</h2>
 
-      <div className="flex flex-row gap-4 items-center">
-        <div className="w-fit">
+      <div className="flex flex-col gap-2">
+        <div className="group w-fit cursor-pointer">
           {updatedProfile.avatar_src ? (
             <Image
               src={updatedProfile.avatar_src || ''}
               alt="User Avatar"
               width={100}
               height={100}
+              className="rounded-full transition-opacity duration-200 group-hover:opacity-75"
             />
           ) : (
-            <div className="w-20 h-20 border-4 border-gray-800 rounded-full">
-              <div className="h-full text-center text-white flex items-center justify-center font-medium">
+            <div className="h-20 w-20 rounded-full border-4 border-gray-800 transition-opacity duration-200 group-hover:border-gray-600">
+              <div className="flex h-full items-center justify-center text-center font-medium text-white">
                 {updatedProfile.first_name[0]}
                 {updatedProfile.last_name[0]}
               </div>
@@ -79,11 +80,11 @@ const UserSettingsForm = ({ profile }: { profile: ProfileType }) => {
           <p className="text-xs">PNG, JPEG under 15MB</p>
         </div>
 
-        <div className="flex flex-row gap-2 ml-auto">
+        <div className="ml-auto flex flex-row gap-2">
           <ToolTip
             triggerContent={
               <div>
-                <Button type="button" className="text-xs h-8" disabled>
+                <Button type="button" className="h-8 text-xs" disabled>
                   Upload new picture
                 </Button>
               </div>
@@ -97,7 +98,7 @@ const UserSettingsForm = ({ profile }: { profile: ProfileType }) => {
               <div>
                 <Button
                   type="button"
-                  className="text-xs h-8"
+                  className="h-8 text-xs"
                   variant="secondary"
                   disabled
                 >
@@ -111,40 +112,31 @@ const UserSettingsForm = ({ profile }: { profile: ProfileType }) => {
         </div>
       </div>
 
-      <div className="flex flex-col gap-2">
-        <label htmlFor="first_name">First Name</label>
-        <Input
-          id="first_name"
-          type="text"
-          name="first_name"
-          value={updatedProfile.first_name}
-          onChange={handleInputChange}
-        />
+      <div className="flex w-full flex-row gap-4">
+        <div className="flex w-1/2 flex-col gap-1">
+          <label htmlFor="first_name">First Name</label>
+          <Input
+            id="first_name"
+            type="text"
+            name="first_name"
+            value={updatedProfile.first_name}
+            onChange={handleInputChange}
+          />
+        </div>
+
+        <div className="flex w-1/2 flex-col gap-1">
+          <label htmlFor="last_name">Last Name</label>
+          <Input
+            id="last_name"
+            type="text"
+            name="last_name"
+            value={updatedProfile.last_name}
+            onChange={handleInputChange}
+          />
+        </div>
       </div>
 
-      <div className="flex flex-col gap-2">
-        <label htmlFor="last_name">Last Name</label>
-        <Input
-          id="last_name"
-          type="text"
-          name="last_name"
-          value={updatedProfile.last_name}
-          onChange={handleInputChange}
-        />
-      </div>
-
-      <div className="flex flex-col gap-2">
-        <label htmlFor="username">User Name</label>
-        <Input
-          id="username"
-          type="text"
-          name="username"
-          value={updatedProfile.username}
-          onChange={handleInputChange}
-        />
-      </div>
-
-      <div className="flex flex-col gap-2">
+      <div className="flex flex-col gap-1">
         <label htmlFor="email">Email</label>
         <Input
           id="email"
