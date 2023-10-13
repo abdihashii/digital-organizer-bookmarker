cc8782862eae1b5f76fddab958c55d6aa57496ce
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2023-10-13 17:08:01 -0500

    Finished avatar uploading flow plus loading states. TODO: delete avatar

--- digi-organizer-app/src/components/Settings/UserSettingsForm.tsx
+++ digi-organizer-app/src/components/Settings/UserSettingsForm.tsx
@@ -9,11 +9,14 @@ import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
 import { useRouter } from 'next/navigation';
 import ToolTip from '../Tooltip';
 import { blobToBase64 } from '@/lib/utils';
+import { Loader2 } from 'lucide-react';
 
 const UserSettingsForm = ({ profile }: { profile: ProfileType }) => {
   const supabase = createClientComponentClient();
   const router = useRouter();
   const [updatedProfile, setUpdatedProfile] = useState<ProfileType>(profile);
+  const [isImageUploading, setIsImageUploading] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false);
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
@@ -25,6 +28,8 @@ const UserSettingsForm = ({ profile }: { profile: ProfileType }) => {
   };
 
   const handleImageChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    setIsImageUploading(true);
+
     const { files } = e.target;
 
     if (files) {
@@ -33,12 +38,14 @@ const UserSettingsForm = ({ profile }: { profile: ProfileType }) => {
       // check if file is an image
       if (!file.type.includes('image')) {
         alert('File must be an image');
+        setIsImageUploading(false);
         return;
       }
 
       // check if file upload is greater than 5MB
       if (file.size >= 5000000) {
         alert('File size must be less than 5MB');
+        setIsImageUploading(false);
         return;
       }
 
@@ -57,33 +64,45 @@ const UserSettingsForm = ({ profile }: { profile: ProfileType }) => {
 
       if (error) {
         console.error(error);
+        setIsImageUploading(false);
         return;
       }
 
       alert(cloudinary_image_secure_url);
+
+      setUpdatedProfile((prevState) => ({
+        ...prevState,
+        avatar_src: cloudinary_image_secure_url,
+      }));
+
+      setIsImageUploading(false);
     }
   };
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    setIsSubmitting(true);
+
     e.preventDefault();
 
     const updated_at = new Date().toISOString();
 
-    const { data, error } = await supabase
+    const { error } = await supabase
       .from('profiles')
       .update({
         first_name: updatedProfile.first_name,
         last_name: updatedProfile.last_name,
         username: updatedProfile.username,
         email: updatedProfile.email,
+        avatar_src: updatedProfile.avatar_src,
         updated_at,
       })
-      .eq('id', updatedProfile.id)
-      .select();
+      .eq('id', updatedProfile.id);
 
     if (error) {
       alert(JSON.stringify(error, null, 2));
     }
+
+    setIsSubmitting(false);
 
     router.refresh();
   };
@@ -99,19 +118,27 @@ const UserSettingsForm = ({ profile }: { profile: ProfileType }) => {
       {/* User Avatar stuff */}
       <div className="flex flex-row items-center gap-4">
         <div className="w-fit">
-          {updatedProfile.avatar_src ? (
-            <Image
-              src={updatedProfile.avatar_src || ''}
-              alt="User Avatar"
-              width={100}
-              height={100}
-              className="rounded-full transition-opacity duration-200 group-hover:opacity-75"
-            />
+          {!isImageUploading ? (
+            updatedProfile.avatar_src ? (
+              <Image
+                src={updatedProfile.avatar_src || ''}
+                alt="User Avatar"
+                width={100}
+                height={100}
+                className="rounded-full transition-opacity duration-200 group-hover:opacity-75"
+              />
+            ) : (
+              <div className="h-20 w-20 rounded-full border-4 border-gray-800">
+                <div className="flex h-full items-center justify-center text-center font-medium text-white">
+                  {updatedProfile.first_name[0]}
+                  {updatedProfile.last_name[0]}
+                </div>
+              </div>
+            )
           ) : (
             <div className="h-20 w-20 rounded-full border-4 border-gray-800">
               <div className="flex h-full items-center justify-center text-center font-medium text-white">
-                {updatedProfile.first_name[0]}
-                {updatedProfile.last_name[0]}
+                <Loader2 size={28} className="animate-spin text-white" />
               </div>
             </div>
           )}
@@ -144,6 +171,7 @@ const UserSettingsForm = ({ profile }: { profile: ProfileType }) => {
         </div>
       </div>
 
+      {/* User Info stuff */}
       <div className="flex w-full flex-row gap-4">
         <div className="flex w-1/2 flex-col gap-1">
           <label htmlFor="first_name">First Name</label>
@@ -168,6 +196,19 @@ const UserSettingsForm = ({ profile }: { profile: ProfileType }) => {
         </div>
       </div>
 
+      {/* Username */}
+      <div className="flex flex-col gap-1">
+        <label htmlFor="username">Username</label>
+        <Input
+          id="username"
+          type="text"
+          name="username"
+          value={updatedProfile.username}
+          onChange={handleInputChange}
+        />
+      </div>
+
+      {/* Email */}
       <div className="flex flex-col gap-1">
         <label htmlFor="email">Email</label>
         <Input
@@ -179,12 +220,13 @@ const UserSettingsForm = ({ profile }: { profile: ProfileType }) => {
         />
       </div>
 
-      <pre className="text-sm text-white">
-        {JSON.stringify(updatedProfile, null, 2)}
-      </pre>
-
+      {/* Submit button */}
       <Button type="submit" className="btn btn-primary">
-        Save Changes
+        {isSubmitting ? (
+          <Loader2 size={20} className="animate-spin text-black" />
+        ) : (
+          'Save Changes'
+        )}
       </Button>
     </form>
   );
