f9bc6f99b261fca276a7eb2b7d2fa08ac81884ec
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2023-10-16 15:43:36 -0500

    Fixes build errors

--- digi-organizer-app/src/components/Dashboard/FoldersDashboard.tsx
+++ digi-organizer-app/src/components/Dashboard/FoldersDashboard.tsx
@@ -23,7 +23,10 @@ const FoldersDashboard = ({
   return (
     <article className="min-h-screen flex-1 overflow-y-auto bg-gray-100 p-4 dark:bg-slate-700 md:p-12 lg:w-9/12">
       <div className="flex flex-col gap-8 lg:w-10/12">
-        <SearchBookmarks user={user} setFolderList={setFolderList} />
+        {/* <SearchBookmarks
+          user={user}
+          // setFolderList={setFolderList}
+        /> */}
 
         <FolderList folders={folderList} />
       </div>
