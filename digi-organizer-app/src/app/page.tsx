998b0ddead3e6d7c804c33e406fab631a5377abb
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2023-10-16 13:43:52 -0500

    Home page gives the user an option to go to dashboard when signed in

--- digi-organizer-app/src/app/page.tsx
+++ digi-organizer-app/src/app/page.tsx
@@ -1,16 +1,28 @@
+import { getUser } from '@/lib/supabaseServerClient';
 import Link from 'next/link';
 
-export default function Home() {
+export default async function Home() {
+  const user = await getUser();
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-center gap-8 p-24">
       <h1 className="text-4xl font-bold">Digi Organizer</h1>
 
-      <Link
-        className="rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-600"
-        href="/sign-in"
-      >
-        Sign In
-      </Link>
+      {user ? (
+        <Link
+          href="/bookmarks"
+          className="rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-600"
+        >
+          Dashboard
+        </Link>
+      ) : (
+        <Link
+          className="rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-600"
+          href="/sign-in"
+        >
+          Sign In
+        </Link>
+      )}
     </main>
   );
 }
