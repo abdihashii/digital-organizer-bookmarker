39da73d3832dcfaf5b57c1851eec76169ccc2464
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2023-12-04 14:14:56 -0600

    now we make the sign up form look good

--- digi-organizer-app/src/components/Auth/SignUp.tsx
+++ digi-organizer-app/src/components/Auth/SignUp.tsx
@@ -5,27 +5,20 @@ import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Button } from '@/components/ui/button';
 import Link from 'next/link';
-import * as yup from 'yup';
-import { yupResolver } from '@hookform/resolvers/yup';
 import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
 import { useForm } from 'react-hook-form';
 import { Eye, EyeOff, RefreshCw } from 'lucide-react';
 import { FcGoogle } from 'react-icons/fc';
 import { BsGithub } from 'react-icons/bs';
+import Image from 'next/image';
+import { signUpSchema } from '@/schemas';
+import { zodResolver } from '@hookform/resolvers/zod';
 
 type FormData = {
   email: string;
   password: string;
   confirmPassword: string;
 };
-
-const schema = yup
-  .object({
-    email: yup.string().email().required(),
-    password: yup.string().required(),
-    confirmPassword: yup.string().required(),
-  })
-  .required();
 
 export default function SignUpPage() {
   const supabase = createClientComponentClient();
@@ -35,12 +28,16 @@ export default function SignUpPage() {
     handleSubmit,
     formState: { errors },
   } = useForm<FormData>({
-    resolver: yupResolver(schema),
+    resolver: zodResolver(signUpSchema),
   });
 
   const [showPassword, setShowPassword] = useState(false);
   const [showConfirmPassword, setShowConfirmPassword] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState({
+    password: false,
+    google: false,
+    github: false,
+  });
 
   // const handleSignUpWithGitHub = async () => {
   //   try {
@@ -54,7 +51,10 @@ export default function SignUpPage() {
 
   const handleSignUp = async (formData: FormData) => {
     // loading spinner for sign up button. This is a local state
-    setIsLoading(true);
+    setIsLoading({
+      ...isLoading,
+      password: true,
+    });
 
     try {
       const { data } = await supabase.auth.signUp({
@@ -67,7 +67,10 @@ export default function SignUpPage() {
       alert(JSON.stringify(error.message, null, 2));
       // TODO: Show a user-friendly error message on the UI
     } finally {
-      setIsLoading(false);
+      setIsLoading({
+        ...isLoading,
+        password: false,
+      });
     }
   };
 
@@ -89,17 +92,18 @@ export default function SignUpPage() {
 
   return (
     <main className="h-screen w-full md:flex md:flex-row md:items-center lg:p-20 xl:p-28">
-      <article className="hidden h-full w-2/3 items-center bg-gray-100 px-10 dark:bg-gray-500 md:flex">
-        <p className="text-3xl font-bold text-gray-900">
+      <article className="relative hidden h-full w-2/3 items-center bg-gray-100 px-10 dark:bg-gray-500 md:flex">
+        {/* <p className="text-3xl font-bold text-gray-900">
           Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        </p>
+        </p> */}
 
-        {/* <Image
-					src="/ben-kim-HeSAWmZ_tqw-unsplash.jpg"
-					width={'100%'}
-					height={'100%'}
-					alt="a snow covered mountain with a sky background"
-				/> */}
+        <Image
+          // src="/ben-kim-HeSAWmZ_tqw-unsplash.jpg"
+          src="https://images.unsplash.com/photo-1696339434901-cf4728e1223e?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          fill={true}
+          objectFit="cover"
+          alt="a snow covered mountain with a sky background"
+        />
       </article>
 
       <article className="flex h-full flex-col justify-center gap-8 px-10 md:w-1/2">
@@ -156,8 +160,12 @@ export default function SignUpPage() {
           </section>
 
           <section>
-            <Button className="w-full" type="submit" disabled={isLoading}>
-              {isLoading ? (
+            <Button
+              className="w-full"
+              type="submit"
+              disabled={isLoading.password}
+            >
+              {isLoading.password ? (
                 <>
                   <RefreshCw className="mr-2 animate-spin" />
                   Signing up...
@@ -176,8 +184,8 @@ export default function SignUpPage() {
         </section>
 
         <section className="flex flex-col gap-8">
-          <Button disabled={isLoading}>
-            {isLoading ? (
+          <Button disabled={isLoading.google}>
+            {isLoading.google ? (
               <>
                 <RefreshCw className="mr-2 animate-spin" />
                 Signing up...
@@ -191,10 +199,10 @@ export default function SignUpPage() {
           </Button>
 
           <Button
-            disabled={isLoading}
+            disabled={isLoading.github}
             // onClick={handleSignUpWithGitHub}
           >
-            {isLoading ? (
+            {isLoading.github ? (
               <>
                 <RefreshCw className="mr-2 animate-spin" />
                 Signing up...
--- digi-organizer-app/src/schemas/index.ts
+++ digi-organizer-app/src/schemas/index.ts
@@ -8,3 +8,13 @@ export const signInSchema = z.object({
     message: 'Password must be at least 4 characters long',
   }),
 });
+
+export const signUpSchema = z.object({
+  email: z.string().email({
+    message: 'Please enter a valid email address',
+  }),
+  password: z.string().min(4, {
+    message: 'Password must be at least 4 characters long',
+  }),
+  confirmPassword: z.string(),
+});
