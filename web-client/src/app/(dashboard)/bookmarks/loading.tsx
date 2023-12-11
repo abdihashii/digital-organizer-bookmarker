42b619fe1fd354a5098cb04402ba905c1182c72a
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2023-12-10 20:16:05 -0600

    Renaming some loading files

--- web-client/src/app/(dashboard)/bookmarks/loading.tsx
+++ web-client/src/app/(dashboard)/bookmarks/loading.tsx
@@ -1,6 +1,6 @@
 import { Skeleton } from '@/components/ui/skeleton';
 
-export default function LoadingDashboardPage() {
+export default function LoadingBookmarksPage() {
   return (
     <article className="min-h-screen flex-1 overflow-y-auto bg-gray-100 p-4 dark:bg-slate-700 md:p-12 lg:w-9/12">
       <div className="flex flex-col gap-8 lg:w-10/12">
