6d44e4352400808043ba3581c056b077107d05eb
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2024-02-12 18:01:41 -0600

    Add a util file and async function for finding OG metadata from a URL

---
+++ sunjoy/src/utils/metadata.ts
@@ -0,0 +1,23 @@
+import ogs from "open-graph-scraper";
+
+export const generateOpenGraphMetadata = async (url: string) => {
+  try {
+    const response = await ogs({ url });
+
+    if (response.error) {
+      throw response.error;
+    }
+
+    const { result } = response;
+
+    const { ogImage } = result;
+
+    if (!ogImage) {
+      throw new Error("No ogImage found");
+    }
+
+    return ogImage[0].url;
+  } catch (error) {
+    return error;
+  }
+};
