69ffd697792c3a4b660721242398ab2b4ccc4461
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2024-02-14 12:16:06 -0600

    format AddBookmarkForm with linter and prettier

--- sunjoy/src/components/AddBookmarkForm.tsx
+++ sunjoy/src/components/AddBookmarkForm.tsx
@@ -1,38 +1,41 @@
-"use client";
+'use client';
 
-import React from "react";
-import { useFormState, useFormStatus } from "react-dom";
-import { addBookmark } from "@/app/actions";
+import React from 'react';
+import { useFormState, useFormStatus } from 'react-dom';
+import { addBookmark } from '@/app/actions';
 
 const Submit = () => {
-  const { pending } = useFormStatus();
+	const { pending } = useFormStatus();
 
-  return (
-    <button
-      className={`bg-black text-white hover:bg-slate-500 rounded-lg px-4 py-2 ${pending ? "cursor-not-allowed bg-slate-500" : ""}`}
-      type="submit"
-      aria-disabled={pending}
-    >
-      {pending ? "Adding..." : "Add"}
-    </button>
-  );
+	return (
+		<button
+			className={`rounded-lg bg-black px-4 py-2 text-white hover:bg-slate-500 ${pending ? 'cursor-not-allowed bg-slate-500' : ''}`}
+			type="submit"
+			aria-disabled={pending}
+		>
+			{pending ? 'Adding...' : 'Add'}
+		</button>
+	);
 };
 
 const AddBookmarkForm = () => {
-  const [, formAction] = useFormState(addBookmark, null);
+	const [, formAction] = useFormState(addBookmark, null);
 
-  return (
-    <form action={formAction} className="space-x-2">
-      <input
-        className="border-2 border-slate-500 rounded-lg p-2"
-        type="text"
-        placeholder="URL"
-        name="url"
-      />
+	return (
+		<form
+			action={formAction}
+			className="w-1/4 space-x-2"
+		>
+			<input
+				className="w-10/12 rounded-lg border-2 border-slate-500 p-2"
+				type="text"
+				placeholder="URL"
+				name="url"
+			/>
 
-      <Submit />
-    </form>
-  );
+			<Submit />
+		</form>
+	);
 };
 
 export default AddBookmarkForm;
