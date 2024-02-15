9a9890e2a03474cc621c87a2cf19fd9bb4b65f9a
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2024-02-14 19:00:54 -0600

    Add delete bookmark functionality, but toast message is not working for some reason.

--- sunjoy/src/components/BookmarkCard.tsx
+++ sunjoy/src/components/BookmarkCard.tsx
@@ -1,16 +1,66 @@
+'use client';
+
+import { useFormState, useFormStatus } from 'react-dom';
 import Link from 'next/link';
-import React from 'react';
-import GenerateTagsButton from './GenerateTagsButton';
+import React, { useEffect, useRef } from 'react';
 import Image from 'next/image';
+import { deleteBookmark } from '@/app/actions';
+
+import GenerateTagsButton from './GenerateTagsButton';
+import { Button } from './ui/button';
+import { MoreVertical, Trash2, CheckCircle, AlertCircle } from 'lucide-react';
+import { Popover, PopoverContent, PopoverTrigger } from './ui/popover';
+import { useToast } from './ui/use-toast';
+
+const DeleteBookmarkButton = () => {
+	const { pending } = useFormStatus();
+
+	return (
+		<Button
+			variant="ghost"
+			className={`flex items-center gap-1 ${pending && 'cursor-not-allowed'}`}
+			type="submit"
+			aria-disabled={pending}
+		>
+			{pending ?
+				'Deleting...'
+			:	<>
+					<Trash2 /> Delete
+				</>
+			}
+		</Button>
+	);
+};
 
 const BookmarkCard = ({ bookmark }: { bookmark: Bookmark }) => {
+	const [formState, formAction] = useFormState(deleteBookmark, {
+		success: false,
+		message: '',
+	});
+	const { toast } = useToast();
+	const formRef = useRef<HTMLFormElement>(null);
+
+	useEffect(() => {
+		// Only show toast if the form has been submitted and there is a message + success
+		if (formState.message) {
+			toast({
+				description: (
+					<p className="flex items-center gap-4">
+						{formState.success ?
+							<CheckCircle className="text-green-500" />
+						:	<AlertCircle />}
+						{formState.message}
+					</p>
+				),
+				variant: formState.success ? 'default' : 'destructive',
+			});
+		}
+	}, [formState, toast]);
+
 	return (
-		<li>
-			<Link
-				className="flex h-[400px] flex-col justify-between rounded-lg border-2 border-slate-500 p-4"
-				href={bookmark.url}
-				target="_blank"
-			>
+		<li className="flex h-[425px] flex-col gap-2 rounded-lg border-2 border-slate-500 p-4">
+			{/* Header */}
+			<div className="flex justify-between gap-4">
 				{bookmark.tags ?
 					<ul className="flex gap-2 overflow-x-scroll">
 						{bookmark.tags.map((tag) => (
@@ -24,7 +74,43 @@ const BookmarkCard = ({ bookmark }: { bookmark: Bookmark }) => {
 					</ul>
 				:	<GenerateTagsButton bookmark={bookmark} />}
 
-				<h3 className="line-clamp-2 text-xl font-medium">{bookmark.title}</h3>
+				<Popover>
+					<PopoverTrigger>
+						<Button
+							variant={'outline'}
+							size="icon"
+						>
+							<MoreVertical />
+						</Button>
+					</PopoverTrigger>
+					<PopoverContent className="w-fit p-2">
+						<form
+							action={formAction}
+							ref={formRef}
+							className="flex flex-col gap-2"
+						>
+							<input
+								type="hidden"
+								name="uuid"
+								value={bookmark.uuid}
+							/>
+
+							{/* <Button variant="ghost">Edit</Button> */}
+							<DeleteBookmarkButton />
+						</form>
+					</PopoverContent>
+				</Popover>
+			</div>
+
+			{/* Content */}
+			<Link
+				href={bookmark.url}
+				target="_blank"
+				className="flex h-full flex-col justify-between"
+			>
+				<h3 className="mb-auto line-clamp-2 text-xl font-medium">
+					{bookmark.title}
+				</h3>
 
 				{bookmark.imgsrc && (
 					<div className="relative h-56 w-full">
