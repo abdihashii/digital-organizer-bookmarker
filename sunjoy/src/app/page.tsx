49e4344b786c8cbe3236c155c28ffeedd77a3167
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2024-02-19 23:31:58 -0600

    clean up code

--- sunjoy/src/app/page.tsx
+++ sunjoy/src/app/page.tsx
@@ -1,10 +1,11 @@
-import SignOutClientComponent from '@/components/Auth/SignOutClientComponent';
-import Bookmarks from '@/components/Bookmarks';
 import {
 	createServerSupabaseClient,
 	getUser,
 } from '@/lib/supabaseServerClient';
 import { redirect } from 'next/navigation';
+
+import SignOutClientComponent from '@/components/Auth/SignOutClientComponent';
+import Bookmarks from '@/components/Bookmarks';
 
 export default async function Home() {
 	const user = await getUser();
@@ -15,9 +16,17 @@ export default async function Home() {
 
 	const supabase = createServerSupabaseClient();
 
-	const { data, error } = await supabase
+	const {
+		data,
+		error,
+		// count
+	} = await supabase
 		.from('bookmarks')
-		.select('*')
+		.select(
+			'*',
+			// { count: 'exact' }
+		) // This requests the exact count of all matching rows
+		// .range(0, 5) // Limit to the first 6 bookmarks
 		.order('created_at', { ascending: false });
 
 	if (error) {
@@ -26,7 +35,7 @@ export default async function Home() {
 
 	return (
 		<main className="flex flex-col items-center gap-10 p-24">
-			<section className="flex w-fit flex-col items-center gap-4 border-b border-b-slate-500 p-6">
+			<section className="flex w-full flex-col items-center gap-4 border-b border-b-slate-500 p-6">
 				<h1 className="text-4xl font-semibold">Welcome to SunJoy!</h1>
 
 				<p>{user.email}!</p>
