149b6055b9ea57a21edc08592667f8f4932910cd
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2024-02-26 12:09:45 -0600

    Fix hydration error

--- sunjoy/src/components/BookmarkCard.tsx
+++ sunjoy/src/components/BookmarkCard.tsx
@@ -75,13 +75,8 @@ const BookmarkCard = ({ bookmark }: { bookmark: Bookmark }) => {
 				:	<GenerateTagsButton bookmark={bookmark} />}
 
 				<Popover>
-					<PopoverTrigger>
-						<Button
-							variant={'outline'}
-							size="icon"
-						>
-							<MoreVertical />
-						</Button>
+					<PopoverTrigger className="inline-flex h-10 w-10 items-center justify-center whitespace-nowrap rounded-md border border-input bg-background text-sm font-medium ring-offset-background transition-colors hover:bg-accent hover:text-accent-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50">
+						<MoreVertical />
 					</PopoverTrigger>
 					<PopoverContent className="w-fit p-2">
 						<form
